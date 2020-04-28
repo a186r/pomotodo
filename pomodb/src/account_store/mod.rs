373146eba0e617e7fefdf19372bbbaa738a64996
a186r <hi.aewalle@gmail.com>
2020-04-28 18:08:15 +0800

    试用postgres示例

--- pomodb/src/account_store/mod.rs
+++ pomodb/src/account_store/mod.rs
@@ -1,1 +1,32 @@
 extern crate postgres;
+
+use postgres::{Client, NoTls};
+
+fn main() {
+    let mut client = Client::connect("host=localhost user=postgres", NoTls)?;
+
+    client.batch_execute(
+        "
+    CREATE TABLE person (
+        id      SERIAL PRIMARY KEY,
+        name    TEXT NOT NULL,
+        data    BYTEA
+    )
+        ",
+    )?;
+
+    let name = "Ferris";
+    let data = None::<&[u8]>;
+
+    client.execute(
+        "INSERT INTO person (name, data) VALUES ($1, $2)",
+        &[&name, &data],
+    );
+
+    for row in client.query("SELECT id, name, data FROM person", &[])? {
+        let id = row.get(0);
+        let name = row.get(1);
+        let data = row.get(2);
+        println!("found person: {} {} {:?}", id, name, data);
+    }
+}
