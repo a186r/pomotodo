3d44e7b0c2567d6ba32540dd52f3c8a776175c97
a186r <hi.aewalle@gmail.com>
2020-04-30 12:26:43 +0800

    postgres示例

--- pomodb/src/account_store/mod.rs
+++ pomodb/src/account_store/mod.rs
@@ -1,32 +1,45 @@
 extern crate postgres;
 
 use postgres::{Client, NoTls};
+#[derive(Debug)]
+struct Person{
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>
+}
 
+#[allow(dead_code)]
 fn main() {
-    let mut client = Client::connect("host=localhost user=postgres", NoTls)?;
+    let mut client = match Client::connect("host=localhost user=postgres", NoTls) {
+        Ok(client) => client,
+        Err(e) => {
+            println!("hell {}", e);
+            return
+        }
+    };
 
-    client.batch_execute(
-        "
-    CREATE TABLE person (
-        id      SERIAL PRIMARY KEY,
-        name    TEXT NOT NULL,
-        data    BYTEA
-    )
-        ",
-    )?;
+    client.batch_execute("
+        CREATE TABLE person (
+            id      SERIAL PRIMARY KEY,
+            name    TEXT NOT NULL,
+            data    BYTEA
+        )
+    ").expect("Table creation failed");
 
-    let name = "Ferris";
-    let data = None::<&[u8]>;
+    let me = Person{
+        id: 0,
+        name: "Steven".to_owned(),
+        data: None
+    };
 
-    client.execute(
-        "INSERT INTO person (name, data) VALUES ($1, $2)",
-        &[&name, &data],
-    );
+    client.execute("INSERT INTO person (name, data) VALUES ($1, $2)", &[&me.name, &me.data]).expect("INSERT failed");
 
-    for row in client.query("SELECT id, name, data FROM person", &[])? {
-        let id = row.get(0);
-        let name = row.get(1);
-        let data = row.get(2);
-        println!("found person: {} {} {:?}", id, name, data);
+    for row in &client.query("SELETE id, name, data FROM person", &[]).unwrap(){
+        let person = Person{
+            id: row.get(0),
+            name: row.get(1),
+            data: row.get(2),
+        };
+        println!("Found person {:?}", person);
     }
 }
