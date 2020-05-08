fa635c3f0eff802c77e9795ded0b9b604d434419
a186r <hi.aewalle@gmail.com>
2020-05-08 22:52:16 +0800

    使用keccak256将message做hash

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,15 +1,14 @@
 use ethkey::prelude::*;
+use ethsign_crypto::Keccak256;
 
 pub fn test_key() {
     let key = EthAccount::load_or_generate("./res/keystore", "passwd")
         .expect("should load or generate new eth key");
     println!("{:?}", key.address());
 
-    let message = [
-        9_u8, 1_u8, 2_u8, 'e' as u8, 'c' as u8, 8_u8, 0_u8, 3_u8, 'b' as u8, 2_u8, 'c' as u8,
-        'e' as u8, 4_u8, 9_u8, 'e' as u8, 4_u8, 'a' as u8, 5_u8, 4_u8, 1_u8, 0_u8, 6_u8, 8_u8,
-        'd' as u8, 4_u8, 9_u8, 5_u8, 'a' as u8, 'b' as u8, 5_u8, 7_u8, 0_u8,
-    ];
+    let str = String::from("test string");
+    
+    let message = Keccak256::keccak256(str.as_bytes());
 
     let signature = key.sign(&message).unwrap();
 
